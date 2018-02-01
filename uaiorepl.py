0606874ea12d443f027682bd72980d4f85f6f7de
shawwwn_ucd <xgwang@ucdavis.edu>
2018-02-01 05:22:37 -0800

    add uaiorepl.py

---
+++ uaiorepl.py
@@ -0,0 +1,62 @@
+import machine
+import uio
+import sys
+import uasyncio
+import gc
+
+ua = machine.UART(0)
+ua.init()
+
+async def start(prompt=b">>>> ", cont_prompt=b".... "):
+	ss = uio.StringIO()
+
+	# get user input
+	while True:
+		# reset internal & external stream cursor
+		ss.seek(0)
+		ss_cur = 0
+
+		sys.stdout.write(prompt)
+
+		# one character at a time
+		while True:
+			if ua.any():
+				ch = ua.read(1)
+				asc2 = ord(ch)
+
+				if 31<asc2<127: # ASCII printable characters
+					ss.seek(ss_cur)
+					ss.write(ch)
+					ss_cur += 1
+					sys.stdout.write(ch)
+				elif asc2==127: # DEL(backspace)
+					ss_cur -= 1 if ss_cur>0 else 0
+					sys.stdout.write(ch)
+				elif asc2==13: # CR(return)
+					sys.stdout.write(b'\n')
+					break
+				elif asc2==2 or asc2==10: # STX(ctrl+b) | LF(newline)
+					sys.stdout.write(b'\n')
+					sys.stdout.write(cont_prompt)
+					ss.write(b'\n')
+					ss_cur += 1
+			await uasyncio.sleep_ms(1)
+
+		# read to a string
+		ss.seek(0)
+		src_code = ss.read(ss_cur)
+
+		# Trying to simulate mp_compile() with flag isrepl=true
+		try:
+			ret = eval(src_code)
+			if ret!=None:
+				print(ret)
+		except: # if eval() fail, then exec()
+			try:
+				exec(src_code)
+			except Exception as e:
+				print(e) # display syntax error, etc.
+
+		gc.collect()
+		await uasyncio.sleep_ms(10)
+
